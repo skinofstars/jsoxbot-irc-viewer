b8cf8f8650127f10db15e4a82e1ad26cc6aebf58
Kevin Carmody <kevin@skinofstars.com>
2013-05-02 00:47:54 +0100

    create remoteCouch plugin and change script to use it

--- script.js
+++ script.js
@@ -12,31 +12,40 @@ function IRCView(){
     // **  use subscribed fields to update time
     // * pretty print time - http://bassistance.de/jquery-plugins/jquery-plugin-prettydate/
 
-    // * <strike> build the query using $.couch plugin - eg $db = $.couch.db("chatbot") </strike>
-    // * <strike> add $.couch.db.changes listener if time is current </strike>
-    // cors hell
-    // $.couch.urlPrefix = "//skinofstars.iriscouch.com";
-    // $.couch.config({crossDomain:true, dataType:"JSONP"});</strike>
-
-
-    
-    $.ajax({
-        url: "//skinofstars.iriscouch.com/chatbot/_design/messages_by_date/_view/messages_by_date?startkey=[1367339768307]",
-        type : 'GET',
-        dataType : "JSONP",
-        success: function (data){
-
-            $.each(data.rows, function (index, item) {
-
-                // we could have pushed item.value here
-                // but doing a bit of date conversion 
-                var date = new Date(item.value.time); 
-                self.messages.push({
-                    "name":item.value.name,
-                    "text":item.value.text,
-                    "time":date.toISOString()
-                });
-            });
-        }
+    $rc = $.remoteCouch;
+    $rc.config({
+        host: "http://skinofstars.iriscouch.com/",
+        db: "chatbot"
     });
+
+    //console.log($rc.status());
+
+    var lastDay = new Date() - 24*60*60*1000;
+
+    // this sets up initial view
+    $rc.view('messages_by_date', { 
+        keys: {
+            startkey: '['+lastDay+']'
+        }    
+    }).then(function(data){
+        $.each(data.rows, function (index, item) {
+
+            // we could have pushed item.value here
+            // but doing a bit of date conversion 
+            var date = new Date(item.value.time); 
+            self.messages.push({
+                "name":item.value.name,
+                "text":item.value.text,
+                "time":date.toISOString()
+            });
+        });
+    });
+
+    // setup longpoll and listeners
+    $rc.longpoll('chatbot');
+    $(document).on('longpoll-data', function(data){
+        console.log(data);
+        // TODO .... something
+    });
+
 }
